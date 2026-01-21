86dbc5c73376755d5dd42da91ed99d68e4929981
magomedvr29 <magomedovr13@mail.ru>
2026-01-21 14:59:58 +0300

    homework №4 I completed all the tasks and also made a button that changes color

---
+++ main.js
@@ -0,0 +1,71 @@
+// Покраска всех карточек 
+
+const productCards = document.querySelectorAll('.card-container');
+const changeColorCardAllButton = document.querySelector('#change-color-all-card');
+const grayColorHash = '#a4a4a4ff';
+const purpleColorHash = '#8800b2ff'
+
+changeColorCardAllButton.addEventListener('click', () => {
+  productCards.forEach((card) => card.style.backgroundColor = grayColorHash)
+})
+
+
+// Покраска первой карточки
+
+const firstProductCard = document.querySelector('.card-container');
+const changeColorFirstCardButton = document.querySelector('#change-color-first-card');
+
+changeColorFirstCardButton.addEventListener('click', () => {
+  firstProductCard.style.backgroundColor = purpleColorHash;
+})
+
+// Открыть google
+
+const openGoogleButton = document.querySelector('#open-google');
+
+openGoogleButton.addEventListener('click', openGoogle);
+
+function openGoogle() {
+  const answer = confirm('Вы действительно хотите открыть Google?');
+
+  if (answer === true) {
+    window.open('https://google.com')
+  } else {
+    return;
+  }
+}
+
+// Вывод консоль лог
+
+const outputlogButton = document.querySelector('#output-console-log');
+
+outputlogButton.addEventListener('click', () => outputConsoleLog('ДЗ №4'))
+
+function outputConsoleLog(message) {
+  alert(message)
+  console.log(message)
+}
+
+// Вывод консоль лог при наведении
+
+const headLine = document.querySelector('.main-title');
+
+headLine.addEventListener('mouseover', function() {
+  console.log(headLine.textContent)
+}
+)
+
+// Изменение цвета кнопни
+
+const button = document.querySelector('#change-color-button')
+
+button.addEventListener('click', function() {
+button.classList.toggle('active')
+});
+
+
+
+
+
+
+
