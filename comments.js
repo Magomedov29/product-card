6e5c8a7f2ca57b703550460087c5955c3d5d0340
magomedvr29 <magomedovr13@mail.ru>
2026-04-12 15:02:24 +0300

    ( Homework №7): fixed errors

--- comments.js
+++ comments.js
@@ -7,69 +7,69 @@ export const comments = [
     id: 1,
     name: "id labore ex et quam laborum",
     email: "Eliseo@gardner.biz",
-    body: "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo"
+    body: "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo",
   },
   {
     postId: 1,
     id: 2,
     name: "quo vero reiciendis velit similique earum",
     email: "Jayne_Kuhic@sydney.com",
-    body: "est natus enim nihil est dolore omnis voluptatem numquam"
+    body: "est natus enim nihil est dolore omnis voluptatem numquam",
   },
   {
     postId: 1,
     id: 3,
     name: "odio adipisci rerum aut animi",
     email: "Nikita@garfield.biz",
-    body: "quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati"
+    body: "quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati",
   },
   {
     postId: 1,
     id: 4,
     name: "alias odio sit",
     email: "Lew@alvin.tv",
-    body: "non et atque\noccaecati deserunt quas accusantium unde odit nobis qui"
+    body: "non et atque\noccaecati deserunt quas accusantium unde odit nobis qui",
   },
   {
     postId: 1,
     id: 5,
     name: "vero eaque aliquid doloribus et culpa",
     email: "Hayden@althea.biz",
-    body: "harum non quasi et ratione\ntempore iure ex voluptates in cum"
+    body: "harum non quasi et ratione\ntempore iure ex voluptates in cum",
   },
   {
     postId: 2,
     id: 6,
     name: "et fugit eligendi deleniti quidem qui sint nihil autem",
     email: "Presley.Mueller@myrl.com",
-    body: "doloribus at sed quis culpa deserunt consectetur qui praesentium"
+    body: "doloribus at sed quis culpa deserunt consectetur qui praesentium",
   },
   {
     postId: 2,
     id: 7,
     name: "repellat provident quia iure culpa",
     email: "Dallas@ole.me",
-    body: "maiores sed dolores similique labore et inventore et"
+    body: "maiores sed dolores similique labore et inventore et",
   },
   {
     postId: 2,
     id: 8,
     name: "et omnis dolorem",
     email: "Mallory_Kunze@marie.org",
-    body: "ut voluptatem corrupti velit\nad voluptatem maiores et"
+    body: "ut voluptatem corrupti velit\nad voluptatem maiores et",
   },
   {
     postId: 2,
     id: 9,
     name: "provident id nuncl",
     email: "Meghan_Littel@rene.us",
-    body: "sapiente assumenda molestiae atque\nadipisci laborum distinctio"
+    body: "sapiente assumenda molestiae atque\nadipisci laborum distinctio",
   },
   {
     postId: 2,
     id: 10,
     name: "eaque et iusto iste est aspernatur porro",
     email: "Carmen_Keeling@miama.info",
-    body: "voluptate iusto quis nobis reprehenderit ipsum amet nulla"
+    body: "voluptate iusto quis nobis reprehenderit ipsum amet nulla",
   }
 ];
